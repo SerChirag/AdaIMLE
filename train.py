7decba28a2aa358622fe73049ab732225c17d719
Chirag <chiragvashist007@gmail.com>
2024-02-25 13:05:31 -0800

    Refactor code to conditionally assign cur_snoise based on H.use_snoise flag

--- train.py
+++ train.py
@@ -204,11 +204,10 @@ def train_loop_imle(H, data_train, data_valid, preprocess_fn, imle, ema_imle, lo
                 latents = cur[1][0]
                 _, target = preprocess_fn(x)
                 
-                # if(H.use_snoise):
-                cur_snoise = [s[indices] for s in sampler.selected_snoise]
-
-                # else:
-                #     cur_snoise = [s[indices] for s in sampler.selected_snoise]
+                if(H.use_snoise):
+                    cur_snoise = [s[indices] for s in sampler.selected_snoise]
+                else:
+                    cur_snoise = [s[indices] for s in sampler.selected_snoise]
 
                 stat = training_step_imle(H, target.shape[0], target, latents, cur_snoise, imle, ema_imle, optimizer, sampler.calc_loss)
                 stats.append(stat)
