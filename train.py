db8b65f3e5532c07f3c38c534c20157e0ce05778
Chirag <chiragvashist007@gmail.com>
2025-01-07 00:26:23 +0000

    Update random scales range for multi-resolution loss calculation in training_step_imle

--- train.py
+++ train.py
@@ -42,8 +42,17 @@ def training_step_imle(H, n, targets, latents, snoise, imle, ema_imle, optimizer
     px_z = imle(cur_batch_latents, snoise)
     loss = loss_fn(px_z, targets.permute(0, 3, 1, 2))
 
+    # if(H.use_multi_res):
+    #     step_size = 1 - 0.125 / (H['multi_res_scales'] * 1.0)
+    #     for scale in np.arange(0.125, 1.0, step_size):
+    #         print("Scale: ", scale)
+    #         px_z_scale = 
+    #         targets_scale = F.interpolate(targets.permute(0, 3, 1, 2), scale_factor = scale, antialias=True, mode='bicubic')
+    #         loss_scale = loss_fn(F.interpolate(px_z, scale_factor = scale, antialias=True, mode='bicubic'), targets_scale)
+    #         loss += loss_scale
+
     if(H.use_multi_res):
-        random_scales = np.random.uniform(0.05, 1.0, H['multi_res_scales'])
+        random_scales = np.random.uniform(0.125, 1.0, H['multi_res_scales'])
         for scale in random_scales:
             px_z_scale = F.interpolate(px_z, scale_factor = scale, antialias=True, mode='bicubic')
             targets_scale = F.interpolate(targets.permute(0, 3, 1, 2), scale_factor = scale, antialias=True, mode='bicubic')
