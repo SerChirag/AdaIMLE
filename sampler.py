b532386498103edf11f3eb3ef050c91ee1e99194
Chirag <chiragvashist007@gmail.com>
2025-01-07 00:35:14 +0000

    Add SSIM loss calculation and coefficient to Sampler class; update argument parser

--- sampler.py
+++ sampler.py
@@ -13,12 +13,64 @@ from torch.optim import AdamW
 from helpers.utils import ZippedDataset
 from models import parse_layer_string
 from helpers.angle_sampler import Angle_Generator
+from torch.autograd import Variable
+from math import exp
+
+
+def gaussian(window_size, sigma):
+    gauss = torch.Tensor([exp(-(x - window_size // 2) ** 2 / float(2 * sigma ** 2)) for x in range(window_size)])
+    return gauss / gauss.sum()
+
+def create_window(window_size, channel):
+    _1D_window = gaussian(window_size, 1.5).unsqueeze(1)
+    _2D_window = _1D_window.mm(_1D_window.t()).float().unsqueeze(0).unsqueeze(0)
+    window = Variable(_2D_window.expand(channel, 1, window_size, window_size).contiguous())
+    return window
+
+class SSIM(torch.Module):
+    def __init__(self, window_size=11, size_average=True):
+        super(SSIM, self).__init__()
+        self.window_size = window_size
+        self.size_average = size_average
+    
+    def forward(self, img1, img2):
+        channel = img1.size(-3)
+        window = create_window(self.window_size, channel)
+
+        if img1.is_cuda:
+            window = window.cuda(img1.get_device())
+        window = window.type_as(img1)
+
+        return _ssim(img1, img2, window, self.window_size, channel, self.size_average)
+
+def _ssim(img1, img2, window, window_size, channel, size_average=True):
+    mu1 = F.conv2d(img1, window, padding=window_size // 2, groups=channel)
+    mu2 = F.conv2d(img2, window, padding=window_size // 2, groups=channel)
+
+    mu1_sq = mu1.pow(2)
+    mu2_sq = mu2.pow(2)
+    mu1_mu2 = mu1 * mu2
+
+    sigma1_sq = F.conv2d(img1 * img1, window, padding=window_size // 2, groups=channel) - mu1_sq
+    sigma2_sq = F.conv2d(img2 * img2, window, padding=window_size // 2, groups=channel) - mu2_sq
+    sigma12 = F.conv2d(img1 * img2, window, padding=window_size // 2, groups=channel) - mu1_mu2
+
+    C1 = 0.01 ** 2
+    C2 = 0.03 ** 2
+
+    ssim_map = ((2 * mu1_mu2 + C1) * (2 * sigma12 + C2)) / ((mu1_sq + mu2_sq + C1) * (sigma1_sq + sigma2_sq + C2))
+
+    if size_average:
+        return ssim_map.mean()
+    else:
+        return ssim_map.mean(1).mean(1).mean(1)
 
 class Sampler:
     def __init__(self, H, sz, preprocess_fn):
         self.pool_size = ceil(int(H.force_factor * sz) / H.imle_db_size) * H.imle_db_size
         self.preprocess_fn = preprocess_fn
         self.l2_loss = torch.nn.MSELoss(reduce=False).cuda()
+        self.ssim_loss = SSIM().cuda()
         self.H = H
         self.latent_lr = H.latent_lr
         self.entire_ds = torch.arange(sz)
@@ -246,8 +298,11 @@ class Sampler:
                 #     lpips_feature_loss[bool_mask] = 0.0
 
                 res += torch.sum(lpips_feature_loss, dim=1) / (inp_shape[i] ** 2)
+            
+            ssim_loss = self.ssim_loss(inp, tar)
 
-            loss = self.H.lpips_coef * res.mean() + self.H.l2_coef * l2_loss.mean()
+            loss = self.H.lpips_coef * res.mean() + self.H.l2_coef * l2_loss.mean() + self.H.ssim_coef * ssim_loss
+
             if logging:
                 return loss, res.mean(), l2_loss.mean()
             else:
