c6bcc12717916eb30ab69ceb12cc9ccc376c63b3
Chirag <chiragvashist007@gmail.com>
2025-01-11 23:34:38 -0500

    Update loss calculation in training_step_imle to conditionally apply L2 loss based on input size

--- train.py
+++ train.py
@@ -63,7 +63,10 @@ def training_step_imle(H, n, targets, latents, snoise, imle, ema_imle, optimizer
         for scale in H['multi_res_scales']:
             px_z_scale = F.interpolate(px_z, scale_factor = scale, antialias=True, mode='bicubic')
             targets_scale = F.interpolate(targets.permute(0, 3, 1, 2), scale_factor = scale, antialias=True, mode='bicubic')
-            loss_scale = loss_fn(px_z_scale, targets_scale)
+            if(px_z_scale.shape[2] < 32):
+                loss_scale = loss_fn(px_z_scale, targets_scale, only_l2 = True)
+            else:
+                loss_scale = loss_fn(px_z_scale, targets_scale)
             loss += loss_scale
 
     loss.backward()
