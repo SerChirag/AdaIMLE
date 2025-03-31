4e8400e463d0e58eef3b9bf9636ee6ad9eef6e2b
Chirag <chiragvashist007@gmail.com>
2025-03-31 15:11:51 -0700

    Remove redundant output handling in IMLE forward method

--- models_imle.py
+++ models_imle.py
@@ -138,7 +138,3 @@ class IMLE(nn.Module):
     def forward(self, latents, spatial_noise=None, input_is_w=False):
         x = self.decoder(latents)
         return x[0]
-        out = self.decoder.forward(latents, spatial_noise, input_is_w)
-        # out = torch.clamp(out, -1, 1)
-        return out
-
