0575c1f71793bcd8c46f392b34874ed86433b502
Chirag <chiragvashist007@gmail.com>
2025-04-11 18:55:43 -0700

    Import torch.distributed module for distributed training support in utility functions

--- visual/utils.py
+++ visual/utils.py
@@ -4,6 +4,7 @@ import numpy as np
 import imageio
 import os
 import shutil
+import torch.distributed as dist
 
 def is_dist_avail_and_initialized(): return dist.is_available() and dist.is_initialized()
 def get_world_size(): return dist.get_world_size() if is_dist_avail_and_initialized() else 1
