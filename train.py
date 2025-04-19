9bd5e9a3cf10616a9d42513d7ebb16a4674a1097
Chirag <chiragvashist007@gmail.com>
2025-04-18 22:51:50 -0700

    Adjust visualization frequency in training loop to every 20 epochs for improved performance

--- train.py
+++ train.py
@@ -132,7 +132,7 @@ def train_loop_imle(H, data_train, data_valid, preprocess_fn, imle, ema_imle, lo
         
 
 
-        if (epoch % 5 == 0 and is_main_process()):
+        if (epoch % 20 == 0 and is_main_process()):
             latents = sampler.selected_latents[:H.num_images_visualize]
             with torch.no_grad():
                 generate_for_NN(sampler, split_x_tensor[:H.num_images_visualize], latents,
