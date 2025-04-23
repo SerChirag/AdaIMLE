441b83a3b6dff3db72e27e22bb506f1adb9bf127
Chirag <chiragvashist007@gmail.com>
2025-04-22 18:22:59 -0700

    Refactor distributed training setup; implement init_distributed_mode function and update train loop for clarity

--- helpers/utils.py
+++ helpers/utils.py
@@ -1,3 +1,5 @@
+import argparse
+from datetime import timedelta
 import os
 import json
 import tempfile
@@ -9,6 +11,41 @@ import torch.distributed as dist
 import torch.utils.data as data
 
 import torch.distributed as dist
+
+def init_distributed_mode():
+    if "MASTER_ADDR" in os.environ and "MASTER_PORT" in os.environ:
+        dist_url = f"tcp://{os.environ['MASTER_ADDR']}:{os.environ['MASTER_PORT']}"
+    else:
+        dist_url = "env://"
+
+    # Detect env variables
+    if "RANK" in os.environ and "WORLD_SIZE" in os.environ:
+        rank = int(os.environ["RANK"])
+        world_size = int(os.environ["WORLD_SIZE"])
+        local_rank = int(os.environ.get("LOCAL_RANK", 0))
+        
+    elif "SLURM_NODEID" in os.environ:
+        gpus_per_node = torch.cuda.device_count()
+        node_id = int(os.environ["SLURM_NODEID"])
+        local_rank = int(os.environ["SLURM_LOCALID"])
+        rank = node_id * gpus_per_node + local_rank
+        world_size = int(os.environ["SLURM_NTASKS"])
+    else:
+        raise RuntimeError("Distributed environment not properly set.")
+
+    # Set correct GPU
+    torch.cuda.set_device(local_rank)
+
+    # Initialize DDP
+    dist.init_process_group(
+        backend="nccl",
+        init_method=dist_url,
+        world_size=world_size,
+        rank=rank,
+        timeout=timedelta(seconds=4800)
+    )
+
+    dist.barrier()
 
 def is_dist_avail_and_initialized(): return dist.is_available() and dist.is_initialized()
 def get_world_size(): return dist.get_world_size() if is_dist_avail_and_initialized() else 1
