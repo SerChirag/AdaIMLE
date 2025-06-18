76d5b1229dd93b562ae8eabe057873f66b500593
Chirag <chiragvashist007@gmail.com>
2025-06-18 21:28:30 +0000

    Update default value of --drop_path_rate argument to 0.0 in add_imle_arguments

--- hps.py
+++ hps.py
@@ -209,7 +209,7 @@ def add_imle_arguments(parser):
     parser.add_argument("--se_reduction", type=int, default=16, help="reduction factor for se block")
     parser.add_argument("--dropout_p", type=float, default=0.0, help="dropout rate for convnext block")
 
-    parser.add_argument("--drop_path_rate", type=float, default=0.5, help="dropout rate for convnext block")
+    parser.add_argument("--drop_path_rate", type=float, default=0.0, help="dropout rate for convnext block")
     parser.add_argument("--use_drop_path", default=False, type=lambda x: bool(strtobool(x)))  # whether to use drop_path block
 
     # some metric args
