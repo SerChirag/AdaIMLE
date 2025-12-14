bbe823cbfe200d5bcc0e206bd1dc5d69e9483465
cva19 <cva19@sfu.ca>
2025-12-14 00:00:54 -0800

    Enhance ConvNeXtBlock with additional normalization options and refactor weight initialization

--- models.py
+++ models.py
@@ -6,6 +6,7 @@ from mapping_network import MappingNetowrk, AdaptiveInstanceNorm, NoiseInjection
 from helpers.imle_helpers import get_1x1
 from collections import defaultdict
 import numpy as np
+from timm.layers import trunc_normal_, DropPath
 import itertools
 
 def parse_layer_string(s):
@@ -56,14 +57,21 @@ class ConvNeXtBlock(nn.Module):
     def __init__(self, dim, H, expansion=4, kernel_size=7, use_se=True, reduction=16, dropout=0.0):
         super().__init__()
         self.dw_conv = nn.Conv2d(dim, dim, kernel_size=kernel_size, padding=kernel_size//2, groups=dim)
+
         if(H.convnext_norm == 'layernorm'):
             self.norm = nn.LayerNorm(dim, eps=H.convnext_norm_eps)
         elif(H.convnext_norm == 'rmsnorm'):
             self.norm = nn.RMSNorm(dim, eps=H.convnext_norm_eps)
-        self.pw_conv1 = nn.Conv2d(dim, expansion * dim, kernel_size=1)
+        
+        if(H.convnext_norm == 'layernorm'):
+            self.norm2 = nn.LayerNorm(dim, eps=H.convnext_norm_eps)
+        elif(H.convnext_norm == 'rmsnorm'):
+            self.norm2 = nn.RMSNorm(dim, eps=H.convnext_norm_eps)
+
+        self.pw_conv1 = nn.Linear(dim, expansion * dim)
         self.gelu = nn.GELU()
         self.sigmoid = nn.Sigmoid()
-        self.pw_conv2 = nn.Conv2d(expansion * dim, dim, kernel_size=1)
+        self.pw_conv2 = nn.Linear(expansion * dim, dim)
 
         ## single parameter for residual ratio
         self.use_se = use_se
@@ -72,10 +80,17 @@ class ConvNeXtBlock(nn.Module):
         else:
             # Indentity layer if SE is not used
             self.se = nn.Identity()
-            
-        self.residual_ratio = nn.Parameter(torch.tensor(H.residual_ratio))  
-        self.residual_type = H.residual_type
+
+        self.residual_ratio = nn.Parameter(torch.tensor(H.residual_ratio)) 
+        self.residual_type = H.residual_type  # 'normal' or 'convex' 
         self.dropout = nn.Dropout2d(p=dropout)  # <- NEW LINE
+        self.apply(self._init_weights)
+
+    def _init_weights(self, m):
+        if isinstance(m, (nn.Conv2d, nn.Linear)):
+            trunc_normal_(m.weight, std=.02)
+            if m.bias is not None:
+                nn.init.constant_(m.bias, 0)
 
     
     def forward(self, x):
@@ -85,16 +100,12 @@ class ConvNeXtBlock(nn.Module):
         # Permute to channels-last for LayerNorm
         x = x.permute(0, 2, 3, 1)
         x = self.norm(x)
-        # Permute back to channels-first
-        x = x.permute(0, 3, 1, 2)
-        # Pointwise conv to expand channels
         x = self.pw_conv1(x)
         x = self.gelu(x)
-
-        # Apply dropout
-        # x = self.dropout(x)
-        # Pointwise conv to compress channels back
         x = self.pw_conv2(x)
+        x = self.norm2(x)
+        x = x.permute(0, 3, 1, 2)
+
         x = self.se(x)
 
         if self.residual_type == 'normal':
