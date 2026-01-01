b39d3df2e0a7ce45a81570efbbaa55f363d0b0dd
cva19 <cva19@sfu.ca>
2026-01-01 13:25:55 -0800

    Add nn_search_batch argument for nearest neighbor search configuration

--- hps.py
+++ hps.py
@@ -246,6 +246,8 @@ def add_imle_arguments(parser):
     parser.add_argument("--use_convnext_bias", default=True, type=lambda x: bool(strtobool(x)))  # whether to use se block
     parser.add_argument("--use_convnext_weight", default=False, type=lambda x: bool(strtobool(x)))  # whether to use se block
 
+    parser.add_argument("--nn_search_batch", type=int, default=64, help="batch size for nearest neighbour search")
+
 
     parser.add_argument("--use_se", default=True, type=lambda x: bool(strtobool(x)))  # whether to use se block
     parser.add_argument("--se_reduction", type=int, default=16, help="reduction factor for se block")
