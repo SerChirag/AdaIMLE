Fix fractional loss scaling and update epoch logging conditions in training step

train.py
@@ -51,7 +51,7 @@ def training_step_imle(H, n, targets, latents, imle, ema_imle, optimizer, loss_f
         loss = loss_fn(px_z[-1], targets.permute(0, 3, 1, 2))
         loss_measure = loss.clone()
         if(H.frac_loss):
-            loss = loss * (8 / px_z_scale.shape[2])
+            loss = loss * (8 / px_z[-1].shape[2])
         num_resolutions = 1
 
         if(H.use_multi_res):
@@ -127,7 +127,7 @@ def train_loop_imle(H, data_train, data_valid, preprocess_fn, imle, ema_imle, lo
         safe_barrier()        
 
 
-        if (epoch % 1 == 0 and is_main_process()):
+        if (epoch 20 == 0 and is_main_process()):
             latents = sampler.selected_latents[:H.num_images_visualize]
             with torch.no_grad():
                 imle.eval()
@@ -297,7 +297,7 @@ def train_loop_imle(H, data_train, data_valid, preprocess_fn, imle, ema_imle, lo
                 logprint(model=H.desc, type='train_loss', epoch=epoch, step=iterate, **metrics)
 
 
-        if (epoch % 1 == 0 and is_main_process()):
+        if (epoch % 5 == 0 and is_main_process()):
             imle.eval()
             with torch.no_grad():
                 generate_visualization(H, sampler, viz_batch_original,