168baa10d5d4afabebc7d3982cb8db46897e6326
cva19 <cva19@sfu.ca>
2026-03-05 04:04:31 -0800

    Update huber_delta default value in argument parser and adjust margin calculation in Sampler

--- hps.py
+++ hps.py
@@ -252,7 +252,7 @@ def add_imle_arguments(parser):
     parser.add_argument('--imle_db_topk', type=int, default=20)  # top-k for imle database search
 
     parser.add_argument("--loss_type", default='l2',choices=["l2", "huber"], help="type of loss")
-    parser.add_argument("--huber_delta", type=float, default=0.2, help="delta for huber loss")
+    parser.add_argument("--huber_delta", type=float, default=0.05, help="delta for huber loss")
     
     # some metric args
     parser.add_argument("--space", choices=["z", "w"], help="space that PPL calculated with")
