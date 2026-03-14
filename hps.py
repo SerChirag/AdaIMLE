60ecaefc9e562f649fce20e789ce30ce92925162
cva19 <cva19@sfu.ca>
2026-03-13 20:11:09 -0700

    fix: Update default loss scale to 1.0 for robust losses in argument parser

--- hps.py
+++ hps.py
@@ -255,7 +255,7 @@ def add_imle_arguments(parser):
 
     parser.add_argument("--loss_type", default='l2',choices=["l2", "huber", "pseudo_l1", "cauchy"], help="type of loss")
     parser.add_argument("--huber_delta", type=float, default=0.05, help="delta for huber loss")
-    parser.add_argument("--loss_scale", type=float, default=4.0, help="scale for general robust losses, e.g. pseudo-huber, pseudo-l1, cauchy")
+    parser.add_argument("--loss_scale", type=float, default=1.0, help="scale for general robust losses, e.g. pseudo-huber, pseudo-l1, cauchy")
     
     # some metric args
     parser.add_argument("--space", choices=["z", "w"], help="space that PPL calculated with")
